7145ee15bb6ab3a2f9daa4426904f919fbd4a3d3
Phuong Dao <s4029319@rmit.edu.vn>
2024-01-27 23:57:17 +0700

    :3

---
+++ src/main/java/app/statecity.java
@@ -0,0 +1,90 @@
+package app;
+
+import java.util.ArrayList;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class statecity implements Handler {
+    // URL of this page relative to http://localhost:7001/
+    public static final String URL = "/citystate.html";
+
+    // Name of the HTML file in the resources folder
+    private static final String TEMPLATE = ("citystate.html");
+
+    @Override
+    public void handle(Context context) throws Exception {
+        // Create a simple HTML webpage in a String
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        JDBCConnection jdbc = new JDBCConnection();
+        ArrayList<Integer> yearselection = new ArrayList<Integer>();
+        ArrayList<info> infos = jdbc.getallYear();
+
+        for (info time : infos) {
+            yearselection.add(time.year);
+        }
+        model.put("yearselection", yearselection);
+
+        // DO NOT MODIFY THIS
+        // Makes Javalin render the webpage
+        // COUNTRY SELECTION
+        JDBCConnection jdbc1 = new JDBCConnection();
+        ArrayList<String> countryselection = new ArrayList<String>();
+        ArrayList<info> infos1 = jdbc1.getallCountry();
+
+        for (info time1 : infos1) {
+            countryselection.add(time1.country);
+        }
+        model.put("countryselection", countryselection);
+
+        String country = context.formParam("country");
+        String state = context.formParam("state");
+        String city = context.formParam("city");
+        String startYear = context.formParam("startYear");
+        String endYear = context.formParam("endYear");
+
+        System.out.println(country + " " + state + " " + city + " " + startYear + " " + endYear);
+        System.out.println(state);
+
+        if (state == null || state.isEmpty()) {
+            if (country == null) {
+                model.put("title", new String("City/State View"));
+            } else if (Integer.parseInt(startYear) > Integer.parseInt(endYear)) {
+                model.put("title", new String("Error: Start year must be less than end year"));
+            } else {
+                model.put("title", new String("Viewing " + country + ", " + city));
+            }
+            System.out.println("hehe you choose city");
+            System.out.println(country + " " + city + " " + startYear + " " + endYear);
+            JDBCConnection jbdc3 = new JDBCConnection();
+            ArrayList<info> infos3 = jbdc3.readCityInfo(startYear, endYear, country, city);
+            model.put("tableInfo", infos3);
+        } else if (city == null || city.isEmpty()) {
+            if (country == null) {
+                model.put("title", new String("City/State View"));
+            } else if (Integer.parseInt(startYear) > Integer.parseInt(endYear)) {
+                model.put("title", new String("Error: Start year must be less than end year"));
+            } else {
+                model.put("title", new String("Viewing " + country + ", " + state));
+            }
+            System.out.println("hehe you choose state");
+            System.out.println(country + " " + state + " " + startYear + " " + endYear);
+            JDBCConnection jdbc2 = new JDBCConnection();
+            ArrayList<info> infos2 = jdbc2.readStateInfo(startYear, endYear, country, state);
+            model.put("tableInfo", infos2);
+        } else {
+            System.out.println("Error huhu.");
+        }
+
+        context.render(TEMPLATE, model);
+    }
+}
